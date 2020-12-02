08625df4c2d8fb8251c693864485eee4cdffe59f
chris0611 <christopher.olberg@hotmail.com>
2020-12-02 14:23:43 +0100

    reads input

--- day2/fixpass.c
+++ day2/fixpass.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 256
 
 int main(void) {
 
+    int valid_passes = 0;
 
+    char *str = malloc(MAXLEN);
+    char rule;
+    int min = 0;
+    int max = 0;
 
-  return 0;
+    while(scanf("%d-%d %c: %s\n", &min, &max, &rule, str) == 4) {
+        printf("%d-%d %c: %s\n", min, max, rule, str);
+    }
+
+    return 0;
 }
