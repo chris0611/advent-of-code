993bf80cd21cd521008f4968bb3445ac5c602b50
chris0611 <christopher.olberg@hotmail.com>
2020-12-03 15:00:56 +0100

    solved part 1

--- day3/trees.c
+++ day3/trees.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define WIDTH 32
-#define HEIGTH 323
+#define HEIGHT 323
 
 int main(void) {
     
@@ -9,23 +9,33 @@ int main(void) {
 
     int pos_x = 0, pos_y = 0;
 
-    char **map = malloc(sizeof(char*)*HEIGTH);
+    char **map = malloc(sizeof(char*)*HEIGHT);
     char ch;
 
+    // while-loop reads map into 2D array
+    //
     while ((ch = getchar()) != EOF) {
         if (map[pos_y] == NULL) {
             map[pos_y] = (char*)malloc(sizeof(char)*WIDTH);
         }
         if (ch == '\n') {
-            printf("\n");
             pos_y++;
             pos_x = 0;
         } else {
-            printf("%c", ch);
             map[pos_y][pos_x] = ch;
             pos_x++;
         }
     }
 
+    pos_x = 0;
+
+    for (pos_y = 0; pos_y < HEIGHT; pos_y++) {
+        char pos = map[pos_y][pos_x%(WIDTH-1)];
+        // printf("Position (%d,%d) has: %c\n", pos_x, pos_y, pos);
+        if (pos == '#')
+            treecount++;  
+        pos_x += 3;
+    }    
+    printf("Trees encountered: %d\n", treecount);
     return 0;
 }
