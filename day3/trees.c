ebf9c7d3ef63663f90d00b97a61cf00107e45c56
chris0611 <christopher.olberg@hotmail.com>
2020-12-03 15:13:58 +0100

    solved part 2

--- day3/trees.c
+++ day3/trees.c
@@ -5,12 +5,14 @@
 
 int main(void) {
     
-    int treecount = 0;
-
     int pos_x = 0, pos_y = 0;
 
     char **map = malloc(sizeof(char*)*HEIGHT);
     char ch;
+
+    long long counts[5] = {0};
+    int slope_x[] = {1, 3, 5, 7, 1};
+    int slope_y[] = {1, 1, 1, 1, 2};
 
     // while-loop reads map into 2D array
     //
@@ -27,15 +29,21 @@ int main(void) {
         }
     }
 
-    pos_x = 0;
+    for (int i = 0; i < 5; i++) {
+        pos_x = 0;
+        for (pos_y = 0; pos_y < HEIGHT; pos_y += slope_y[i]) {
+            char pos = map[pos_y][pos_x%(WIDTH-1)];
+            // printf("Position (%d,%d) has: %c\n", pos_x, pos_y, pos);
+            if (pos == '#')
+                counts[i]++;  
+            pos_x += slope_x[i];
+        }
+    }
+    printf("(Part 1) Trees encountered: %d\n", counts[1]);
+    
+    long long product = counts[0] * counts[1] * counts[2] * counts[3] * counts[4];
 
-    for (pos_y = 0; pos_y < HEIGHT; pos_y++) {
-        char pos = map[pos_y][pos_x%(WIDTH-1)];
-        // printf("Position (%d,%d) has: %c\n", pos_x, pos_y, pos);
-        if (pos == '#')
-            treecount++;  
-        pos_x += 3;
-    }    
-    printf("Trees encountered: %d\n", treecount);
+    printf("(Part 2 Products of trees: %lld\n", product);
+
     return 0;
 }
