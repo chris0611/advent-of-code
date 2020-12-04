c1d12307486525cc04b5b1c07640c5479e30c25c
chris0611 <christopher.olberg@hotmail.com>
2020-12-04 16:00:08 +0100

    fixed up part 2

--- day4/passport-2.c
+++ day4/passport-2.c
@@ -3,21 +3,15 @@
 #include <string.h>
 #include <stdbool.h>
 #include <regex.h>
-#include <sys/types.h>
 
 #define BUFSIZE 512
+#define FIELDS 8
 
 int main(void) {
     char input_str[512];
 
     int num_valid = 0;
-    int num_valid_pt2 = 0;
-
-    int num_fields = 0;
-    int num_fields_pt2 = 0;
-
-    char *fields[] = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"};
-    bool matched[8] = {0}; // to avoid matching same field more than once
+    bool matched[FIELDS] = {0}; // to avoid matching same field more than once
 
     // regex for matching each field
     char *constraints[] = {
@@ -27,14 +21,14 @@ int main(void) {
                            "hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)",     // - for height
                            "hcl:#(([0-9])|([a-f])){6}",                             // - for hair color
                            "ecl:(amb|blu|brn|gry|grn|hzl|oth)",                     // - for eye color 
-                           "pid:[0-9]{9}",                                          // - for passport id
-                           "cid:."                                                  // - for country id
+                           "pid:[0-9]{9}( |\n)",                                          // - for passport id
+                           "cid:"                                                   // - for country id
                           };
 
-    regex_t *regex = (regex_t*)malloc(sizeof(regex_t) * 8);     // storing compiled patterns 
+    regex_t *regex = (regex_t*)malloc(sizeof(regex_t) * FIELDS);     // for storing compiled patterns 
 
     // compiling regex patterns
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < FIELDS; i++) {
         if (regcomp(&regex[i], constraints[i], REG_EXTENDED) != 0) {
             fprintf(stderr, "Error during regex compilation\n");
             exit(1);
@@ -45,44 +39,24 @@ int main(void) {
 
         int len = strlen(input_str);
         if (len != 1) {
-            for (int i = 0; i < 8; i++) {
-                if (strstr(input_str, fields[i]) != NULL) {
-                    num_fields++;
-                    if (i == 7)
-                        matched[i] = true; // cid doesn't matter
-                }
-                if (!matched[i] && regexec(&regex[i], input_str, 0, NULL, 0) != REG_NOMATCH) {
+            for (int i = 0; i < FIELDS; i++) {
+                if ((!matched[i]) && (regexec(&regex[i], input_str, 0, NULL, 0) != REG_NOMATCH)) {
                     matched[i] = true;
-                    num_fields_pt2++;
                 }
             }                
-
-            if ((!matched[7] && ((num_fields) == 7)) || (num_fields == 8)) {
-                for (int i = 0; i < 8; i++) {
-                    matched[i] = false;
-                }
-                num_fields = 0;
-                num_valid++;
-            }
-            if (num_fields_pt2 == 8 || (!matched[7] && num_fields_pt2 == 7)) {
-                for (int i = 0; i < 8; i++) {
-                    matched[i] = false;
-                }
-                num_fields_pt2 = 0;
-                num_valid_pt2++;
-
-            }
         } else {
-            num_fields = 0;
-            num_fields_pt2 = 0;
-            for (int i = 0; i < 8; i++) {
-                matched[i] = false;  
+            for (int i = 0; i < FIELDS; i++) {
+                matched[i] = false;
+            }
+        }
+        if (matched[0] && matched[1] && matched[2] && matched[3] && matched[4] && matched[5] && matched[6]) {
+            num_valid += 1;
+            for (int i = 0; i < FIELDS; i++) {
+                matched[i] = false;
             }
         }
     }
-    
-    printf("(Part 1) Number of valid passports: %d\n", num_valid);
-    printf("(Part 2) Number of valid passports: %d\n", num_valid_pt2);
+    printf("(Part 2) Number of valid passports: %d\n", num_valid);
 
     return 0;
 }
