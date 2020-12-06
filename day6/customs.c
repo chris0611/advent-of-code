45f7e8fe58e316fc1089450ca83f6bd855624d4f
chris0611 <christopher.olberg@hotmail.com>
2020-12-06 11:35:27 +0100

    Read input from stdin

--- day6/customs.c
+++ day6/customs.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#define CHARS 26
+#define BUFSIZE 512
 
 int main(void) {
-    
+    uint64_t score = 0;
+
+    char group[BUFSIZE];
+    uint16_t chars_read = 0;
+
+    while (fgets(group, (BUFSIZE - chars_read), stdin) != NULL) {
+        printf("%s", group);
+    }
+        
+
     return 0;
 }
