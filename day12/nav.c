443c6cbf4cd274890a3a7167384cf55b0dd904c8
chris0611 <christopher.olberg@hotmail.com>
2020-12-12 13:47:24 +0100

    Solved Part 2

--- day12/nav.c
+++ day12/nav.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 
-void move(int amt, long *x, long *y, int deg)
+void move_p1(int amt, long *x, long *y, int deg)
 {
     switch(deg) {
         case 0:
@@ -25,11 +25,56 @@ void move(int amt, long *x, long *y, int deg)
     }
 }
 
-int main(void)
+void move_p2(int amt, long *x, long *y, long wp_x, long wp_y)
 {
-    long x = 0;     // east/west
-    long y = 0;     // north/south
-    int deg = 90;   // direction (0 facing directly north, going clockwise)
+    //printf("Moving in the x-axis by: %ld\n", amt * wp_x);
+    *x += amt * wp_x;
+    //printf("Moving in the y-axis by: %ld\n", amt * wp_y);
+    *y += amt * wp_y;
+}
+
+void rotate_wp(long *x, long *y, int deg)
+{
+    //printf("Rotating (%2ld,%2ld) by %d deg\n", *x, *y, deg);
+    long tmp;
+    switch(deg) {
+        case 90:
+            tmp = *x;
+            *x = *y;
+            *y = -tmp;
+            break;
+
+        case 180:
+            *x = -(*x);
+            *y = -(*y);
+            break;
+
+        case 270:
+            tmp = *x;
+            *x = -(*y);
+            *y = tmp;
+            break;
+
+        default:
+          fprintf(stderr, "Invalid number\n");
+          exit(1);
+          break;
+    }
+    //printf("Position after rotating: (%2ld,%2ld)\n", *x, *y);
+}
+
+int main(void)
+{                   
+    long x_1 = 0;   // east/west
+    long y_1 = 0;   // north/south
+    
+    long x_2 = 0;   // same, but for part 2
+    long y_2 = 0;
+
+    long wp_x = 10; // waypoint position relative to ship
+    long wp_y = 1;
+
+    int deg = 90;   // degrees (0 facing directly north, going clockwise)
 
     char c;
     int d;
@@ -37,32 +82,46 @@ int main(void)
     while (scanf("%c%d\n", &c, &d) == 2) {
         switch(c) {
             case 'N':
-                y += d;
+                y_1 += d;
+                wp_y += d;
                 break;
+
             case 'S':
-                y -= d;
+                y_1 -= d;
+                wp_y -= d;
                 break;
+
             case 'E':
-                x += d;
+                x_1 += d;
+                wp_x += d;
                 break;
+
             case 'W':
-                x -= d;
+                x_1 -= d;
+                wp_x -= d;
                 break;
+
             case 'L':
                 if (deg - d < 0)
                     deg = (deg - d + 360);
                 else
                     deg -= d;
+                rotate_wp(&wp_x, &wp_y, 360 - d);
                 break;
+
             case 'R':
                 if (deg + d >= 360)
                     deg = (deg+d - 360);
                 else
                     deg += d;
+                rotate_wp(&wp_x, &wp_y, d);
                 break;
+
             case 'F':
-                move(d, &x, &y, deg);
+                move_p1(d, &x_1, &y_1, deg);
+                move_p2(d, &x_2, &y_2, wp_x, wp_y);
                 break;
+
             default:
                 fprintf(stderr, "Invalid input - '%c'\n", c);
                 exit(1);
@@ -71,7 +130,8 @@ int main(void)
         //printf("(%2ld,%2ld) facing %3d\n",x,y,deg);
     }
 
-    printf("(Part 1) Manhattan distance is: %ld\n", (long)( abs( (double) x ) + abs( (double) y ) ));
+    printf("(Part 1) Manhattan distance is: %ld\n", (long)( abs( (double) x_1 ) + abs( (double) y_1 ) ));
+    printf("(Part 2) Manhattan distance is: %ld\n", (long)( abs( (double) x_2 ) + abs( (double) y_2 ) ));
 
     return 0;
 }
